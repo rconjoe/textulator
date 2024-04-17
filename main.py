07b41de75330861170e9acba5192f7b24a006916
trog <admin@trog.host>
2024-04-17 11:00:44 -0700

    add reactive, add classes and id's to number buttons

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from textual.app import App, ComposeResult
 from textual.containers import Grid
 from textual.widgets import Button, Digits
+from textual.reactive import reactive
 
 
 class CalculatorApp(App[None]):
     CSS_PATH = "main.css"
+
+    number_displayed = reactive("0")
 
     def compose(self) -> ComposeResult:
         yield Digits("1234", id="display")
@@ -13,19 +16,19 @@ class CalculatorApp(App[None]):
             yield Button("+/-", classes="top-button")
             yield Button("%", classes="top-button")
             yield Button.warning("/")
-            yield Button("7")
-            yield Button("8")
-            yield Button("9")
+            yield Button("7", id="number-7", classes="number-button")
+            yield Button("8", id="number-8", classes="number-button")
+            yield Button("9", id="number-9", classes="number-button")
             yield Button.warning("*")
-            yield Button("4")
-            yield Button("5")
-            yield Button("6")
+            yield Button("4", id="number-4", classes="number-button")
+            yield Button("5", id="number-5", classes="number-button")
+            yield Button("6", id="number-6", classes="number-button")
             yield Button.warning("-")
-            yield Button("1")
-            yield Button("2")
-            yield Button("3")
+            yield Button("1", id="number-1", classes="number-button")
+            yield Button("2", id="number-2", classes="number-button")
+            yield Button("3", id="number-3", classes="number-button")
             yield Button.warning("+")
-            yield Button("0", id="number-0")
+            yield Button("0", id="number-0", classes="number-button")
             yield Button(".")
             yield Button.warning("=")
 
