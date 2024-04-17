7c3be5fd96a8b2161d94e366d732e37393db1baa
trog <admin@trog.host>
2024-04-17 11:23:35 -0700

    decrease verbosity, add wip operator handling

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import operator
+
 from textual import on
 from textual.app import App, ComposeResult
 from textual.containers import Grid
@@ -13,27 +15,27 @@ class CalculatorApp(App[None]):
     def compose(self) -> ComposeResult:
         yield Digits("1234", id="display")
         with Grid():
-            yield Button("AC", classes="top-button")
-            yield Button("+/-", classes="top-button")
-            yield Button("%", classes="top-button")
-            yield Button.warning("/")
-            yield Button("7", id="number-7", classes="number-button")
-            yield Button("8", id="number-8", classes="number-button")
-            yield Button("9", id="number-9", classes="number-button")
-            yield Button.warning("*")
-            yield Button("4", id="number-4", classes="number-button")
-            yield Button("5", id="number-5", classes="number-button")
-            yield Button("6", id="number-6", classes="number-button")
-            yield Button.warning("-")
-            yield Button("1", id="number-1", classes="number-button")
-            yield Button("2", id="number-2", classes="number-button")
-            yield Button("3", id="number-3", classes="number-button")
-            yield Button.warning("+")
-            yield Button("0", id="number-0", classes="number-button")
+            yield Button("AC", classes="top")
+            yield Button("+/-", classes="top")
+            yield Button("%", classes="top")
+            yield Button.warning("/", id="truediv", classes="operator")
+            yield Button("7", id="number-7", classes="number")
+            yield Button("8", id="number-8", classes="number")
+            yield Button("9", id="number-9", classes="number")
+            yield Button.warning("*", id="mul", classes="operator")
+            yield Button("4", id="number-4", classes="number")
+            yield Button("5", id="number-5", classes="number")
+            yield Button("6", id="number-6", classes="number")
+            yield Button.warning("-", id="sub", classes="operator")
+            yield Button("1", id="number-1", classes="number")
+            yield Button("2", id="number-2", classes="number")
+            yield Button("3", id="number-3", classes="number")
+            yield Button.warning("+", id="add", classes="operator")
+            yield Button("0", id="number-0", classes="number")
             yield Button(".")
             yield Button.warning("=")
 
-    @on(Button.Pressed, ".number-button")
+    @on(Button.Pressed, ".number")
     def update_number_dispayed(self, event: Button.Pressed) -> None:
         button_id = event.button.id
         _, _, digit = button_id.partition("-")
@@ -41,6 +43,11 @@ class CalculatorApp(App[None]):
 
     def watch_number_displayed(self, new_value: str) -> None:
         self.query_one(Digits).update(new_value)
+
+    @on(Button.Pressed, ".operator")
+    def handle_operator_button(self, event: Button.Pressed) -> None:
+        op_name = event.button.id
+        op = getattr(operator, op_name)
 
 
 if __name__ == "__main__":
